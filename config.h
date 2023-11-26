5a73543d1e813aa5ef5868c40d98e8b6dd3bde1a
Michael Koger <michael.d.koger@gmail.com>
2023-11-26 15:30:50 -0600

    Initial Commit

---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "athw"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "michael.d.koger@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "athw"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "athw 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "athw"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
